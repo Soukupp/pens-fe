50ed6a0bf0ac429fcf65443e15c79e9e301fdd08
Soukupp <1730342751@qq.com>
2024-06-16 12:27:12 +0800

    feat:添加新闻推荐模块

--- src/views/NewsRecommend.vue
+++ src/views/NewsRecommend.vue
@@ -1,18 +1,50 @@
 <template>
     <h class="viewTitle tracking-in-expand">新闻推荐</h>
+    <div class="query-container"> 
+        <el-input v-model="userID" @keyup.enter="handleClickEvent"></el-input>
+        <el-button @click="handleClickEvent" placeholder="请输入您想查询的用户的ID（请注意：用户ID格式为U+数字），按下回车进行查询">查询</el-button>
+    </div>
+    <el-table :data="formData" :header-cell-style="{ 'text-align': 'center' } ">
+        <el-table-column prop="newsId" label="新闻ID" width="180" align="center" />
+        <el-table-column prop="newsTitle" label="新闻标题" width="180" align="center" />
+        <el-table-column prop="newsTheme" label="新闻主题" align="center" />
+        <el-table-column prop="newsLength" label="新闻长度" align="center" />
+        <el-table-column prop="description" label="新闻简介" />
+    </el-table>
 </template>
 
-<script>
-export default {
-    name: "NewsRecommend"
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios from "axios";
+import { ElMessage } from 'element-plus';
+
+
+const userID = ref('');
+const formData = ref(null);
+
+const handleClickEvent = () => {
+    axios.get('https://mock.apipark.cn/m1/4594184-4243668-default/api/recommendNews', {
+        params: {
+            user_id: userID.value,
+        }
+    }).then((res) => {
+        const data = res.data;
+        formData.value = data.response.recommendList;
+    }).catch((err) => {
+        ElMessage({
+            type: 'error',
+            message: err
+        })
+    })
 }
+
 </script>
 
 <style scoped>
 .viewTitle {
     background-image: linear-gradient(96.14deg,
-    rgba(24, 103, 154, 0.69) 0%,
-    #0093bf 100%);
+            rgba(24, 103, 154, 0.69) 0%,
+            #0093bf 100%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     text-align: left;
@@ -23,5 +55,15 @@ export default {
 .tracking-in-expand {
     -webkit-animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
     animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+}
+
+.query-container{
+    display: flex;
+    flex-direction: row;
+    width: 50%;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    gap: 20px;
 }
 </style>
